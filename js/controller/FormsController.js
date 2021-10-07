4626bf1e014df45d9ba10fe1ff0f358ba2f77a7d
joaopedroortiz <joaopedroortiz11@gmail.com>
2021-10-07 11:41:53 -0300

    fix:alinhando html, css e js e atualizando branch principal

--- js/controller/FormsController.js
+++ js/controller/FormsController.js
@@ -1,81 +1,81 @@
-class FormsController{
-  constructor() { 
+class FormsController {
+  constructor() {
     let $ = document.querySelector.bind(document);
     this._formNumber = 1;
-    this._chooseTab1 = $('#chooseTab1');
-    this._chooseTab2 = $('#chooseTab2');
-    this._chooseTab3 = $('#chooseTab3');
-    this._btnNext = $('#btnNext');
-    this._btnFinish = $('#btnFinish');
-    this._tab1 = $('#tab1');
-    this._tab2 = $('#tab2');
-    this._tab3 = $('#tab3');
+    this._chooseTab1 = $("#chooseTab1");
+    this._chooseTab2 = $("#chooseTab2");
+    this._chooseTab3 = $("#chooseTab3");
+    this._btnNext = $("#btnNext");
+    this._btnFinish = $("#btnFinish");
+    this._tab1 = $("#tab1");
+    this._tab2 = $("#tab2");
+    this._tab3 = $("#tab3");
   }
 
-  nextForm(){
+  nextForm() {
     switch (this._formNumber) {
       case 1:
-        if(validationForm1()){
-          this._chooseTab1.classList.remove('choose');
-          this._chooseTab2.classList.add('choose');
-          this._formNumber ++;
+        if (validationForm1()) {
+          this._chooseTab1.classList.remove("choose");
+          this._chooseTab2.classList.add("choose");
+          this._formNumber++;
         }
         break;
       case 2:
         // if(validationForm2()){
-          this._chooseTab2.classList.remove('choose');
-          this._chooseTab3.classList.add('choose');
-          this._btnNext.classList.add('displayNone');
-          this._btnFinish.classList.remove('displayNone');
-          this._formNumber ++;
+        this._chooseTab2.classList.remove("choose");
+        this._chooseTab3.classList.add("choose");
+        this._btnNext.classList.add("displayNone");
+        this._btnFinish.classList.remove("displayNone");
+        this._formNumber++;
         // }
         break;
-      }
     }
+  }
 
-    clickChoice(form){
-      switch (form) {
-        case 'chooseTab1':
-          this._chooseTab2.classList.remove('choose');
-          this._chooseTab3.classList.remove('choose');
-          this._tab1.classList.remove('displayNone');
-          this._tab2.classList.add('displayNone');
-          this._tab3.classList.add('displayNone');
-          this._btnFinish.classList.add('displayNone');
-          this._btnNext.classList.remove('displayNone');
-          this._chooseTab1.classList.add('choose');
-          this._formNumber = 1;
-          break;
-        case 'chooseTab2':
-          if(validationForm1()){
-            this._chooseTab1.classList.remove('choose');
-            this._chooseTab3.classList.remove('choose');
-            this._tab1.classList.add('displayNone');
-            this._tab2.classList.remove('displayNone');
-            this._tab3.classList.add('displayNone');
-            this._btnFinish.classList.add('displayNone');
-            this._btnNext.classList.remove('displayNone');
-            this._chooseTab2.classList.add('choose');
-            this._formNumber = 2;
-          }
-          break;
-        case 'chooseTab3':
-          // if(validationForm1() && validationForm2()){
-            this._chooseTab1.classList.remove('choose');
-            this._chooseTab2.classList.remove('choose');
-            this._chooseTab3.classList.add('choose');
-            this._tab1.classList.add('displayNone');
-            this._tab2.classList.add('displayNone');
-            this._tab3.classList.remove('displayNone');
-            this._btnNext.classList.add('displayNone');
-            this._btnFinish.classList.remove('displayNone');
-            this._formNumber = 3;
-          // }
-          break;
-      }
+  clickChoice(form) {
+    switch (form) {
+      case "chooseTab1":
+        this._chooseTab2.classList.remove("choose");
+        this._chooseTab3.classList.remove("choose");
+        this._tab1.classList.remove("displayNone");
+        this._tab2.classList.add("displayNone");
+        this._tab3.classList.add("displayNone");
+        this._btnFinish.classList.add("displayNone");
+        this._btnNext.classList.remove("displayNone");
+        this._chooseTab1.classList.add("choose");
+        this._formNumber = 1;
+        break;
+      case "chooseTab2":
+        if (validationForm1()) {
+          this._chooseTab1.classList.remove("choose");
+          this._chooseTab3.classList.remove("choose");
+          this._tab1.classList.add("displayNone");
+          this._tab2.classList.remove("displayNone");
+          this._tab3.classList.add("displayNone");
+          this._btnFinish.classList.add("displayNone");
+          this._btnNext.classList.remove("displayNone");
+          this._chooseTab2.classList.add("choose");
+          this._formNumber = 2;
+        }
+        break;
+      case "chooseTab3":
+        // if(validationForm1() && validationForm2()){
+        this._chooseTab1.classList.remove("choose");
+        this._chooseTab2.classList.remove("choose");
+        this._chooseTab3.classList.add("choose");
+        this._tab1.classList.add("displayNone");
+        this._tab2.classList.add("displayNone");
+        this._tab3.classList.remove("displayNone");
+        this._btnNext.classList.add("displayNone");
+        this._btnFinish.classList.remove("displayNone");
+        this._formNumber = 3;
+        // }
+        break;
     }
-    
-    finishForm(){
-      console.log('Implementar Finish');
-    }
+  }
+
+  finishForm() {
+    console.log("Implementar Finish");
+  }
 }
--- js/services/ValidationForms.js
+++ js/services/ValidationForms.js
@@ -5,42 +5,44 @@ Essas serão chamadas no FormsController.js a cada clique dos botões.
 var $ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
 
-function validationForm1(){
-  let validationItems = $$('.input-tab1'); //Seleciona todos os inputs da tab1
+function validationForm1() {
+  let validationItems = $$(".input-tab1"); //Seleciona todos os inputs da tab1
   let flag = true; // Flag de retorno da função
 
   validationItems.forEach((item) => {
-    let validations = { //Variável que contém a validação específica de cada campo e sua msg de aviso, sendo a chave o id do campo
+    let validations = {
+      //Variável que contém a validação específica de cada campo e sua msg de aviso, sendo a chave o id do campo
       nome: {
-        val: verificaString(item.value, ' '),
-        warning: 'Seu nome precisa estar completo!',
+        val: verificaString(item.value, " "),
+        warning: "Seu nome precisa estar completo!",
       },
       nickname: {
-        val:item.value.indexOf(' ') === -1,
-        warning: 'Seu nickname não pode conter espaços em branco!',
+        val: item.value.indexOf(" ") === -1,
+        warning: "Seu nickname não pode conter espaços em branco!",
       },
       email: {
-        val:verificaString(item.value, '@', '.com'),
-        warning: 'Seu e-mail não parece válido!',
+        val: verificaString(item.value, "@", ".com"),
+        warning: "Seu e-mail não parece válido!",
       },
       telefone: {
-        val:!isNaN(item.value.split(' ').join('')),
-        warning: 'Seu telefone deve conter apenas números!',
+        val: !isNaN(item.value.split(" ").join("")),
+        warning: "Seu telefone deve conter apenas números!",
       },
       data: {
-        val:!isNaN(item.value.split('-').join('')),
-        warning: 'Sua data de nascimento não parece válida!',
+        val: !isNaN(item.value.split("-").join("")),
+        warning: "Sua data de nascimento não parece válida!",
       },
       idade: {
-        val:!isNaN(item.value),
-        warning: 'Digite a idade corretamente, apenas números!',
+        val: !isNaN(item.value),
+        warning: "Digite a idade corretamente, apenas números!",
       },
       termos: {
-        val:item.checked,
-        warning: 'Precisamos que você aceite os termos!',
+        val: item.checked,
+        warning: "Precisamos que você aceite os termos!",
       },
-    }
-    if(!validateItem(item, validations[item.name])){ //Testa cada item na função de validação
+    };
+    if (!validateItem(item, validations[item.name])) {
+      //Testa cada item na função de validação
       flag = false; //Caso falhe em algum campo, altera para false a variável de retorno da função
     }
   });
@@ -49,52 +51,55 @@ function validationForm1(){
 
 //TODO:(Matheus Santos) Implementar validação do tab2 aqui
 
-function validateItem(item, validation){ //Função que valida os campos
-  if(item.required){ //Se o item for obrigatório
-    if(item.value !== ''){ //Testa se está preenchido
-    } else { //No caso do campo não ser preenchido o aviso é de que o campo é obrigatório
-      if($(`#${item.id}`).nextElementSibling.classList.value !== 'aviso'){
-        item.insertAdjacentHTML('afterend', `<p class='aviso'>Campo ${item.name} obrigatório</p>`);
+function validateItem(item, validation) {
+  //Função que valida os campos
+  if (item.required) {
+    //Se o item for obrigatório
+    if (item.value !== "") {
+      //Testa se está preenchido
+    } else {
+      //No caso do campo não ser preenchido o aviso é de que o campo é obrigatório
+      if ($(`#${item.id}`).nextElementSibling === null) {
+        item.insertAdjacentHTML(
+          "afterend",
+          `<p class='aviso'>Campo ${item.name} obrigatório</p>`
+        );
       }
       return false;
     }
   }
-  if(validation.val){ //Faz a validação dinamica do campo
+  if (validation.val) {
+    //Faz a validação dinamica do campo
     return true;
-  } else { //Caso falhar na validação dinâmica, passa a exibir a mensagem específica daquele campo
-    if(item.type === 'checkbox'){//Tratamento especial para inputs do tipo checkbox
-      if($(`#${item.id}`).nextElementSibling.nextElementSibling === null){
-        item.nextElementSibling.insertAdjacentHTML('afterend', `<p class='aviso'>${validation.warning}</p>`);
-      }
-    } else {//Tratamento dos demais inputs
-      if($(`#${item.id}`).nextElementSibling.classList.value !== 'aviso'){
-        item.insertAdjacentHTML('afterend', `<p class='aviso'>${validation.warning}</p>`);
-      }
+  } else {
+    //Caso falhar na validação dinâmica, passa a exibir a mensagem específica daquele campo
+    //Tratamento dos demais inputs
+    if ($(`#${item.id}`).nextElementSibling === null) {
+      item.insertAdjacentHTML(
+        "afterend",
+        `<p class='aviso'>${validation.warning}</p>`
+      );
     }
     item.focus();
     return false;
   }
 }
 
-
-function verificaString(word, ...character){ //Função que testa uma string com todos os caracteres passados
-  let flag = true
-  character.forEach(c => { //Testa cada caractere se existe na palavra (var. word)
-    if(word.indexOf(c) === -1){
+function verificaString(word, ...character) {
+  //Função que testa uma string com todos os caracteres passados
+  let flag = true;
+  character.forEach((c) => {
+    //Testa cada caractere se existe na palavra (var. word)
+    if (word.indexOf(c) === -1) {
       flag = false; //Se não encontrar algum caractere passado, altera a flag pra que a função retorne falso
     }
   });
   return flag; //Se todos os caracteres forem encontrados retorna true
 }
 
-function correcaoDoDado(params){ // É chamado toda vez que um campo é alterado no HTML
-  if(params === 'frase'){ //Tratamento diferente para o campo checkbox
-    if($(`#${params}`).nextElementSibling.nextElementSibling !== null){
-      $(`#${params}`).nextElementSibling.nextElementSibling.remove(); //Após a mudança no campo remove esse aviso
-    }
-  } else {
-    if($(`#${params}`).nextElementSibling.classList.value === 'aviso'){
-      $(`#${params}`).nextElementSibling.remove(); //Após a mudança no campo remove esse aviso
-    }
+function correcaoDoDado(params) {
+  // É chamado toda vez que um campo é alterado no HTML
+  if ($(`#${params}`).nextElementSibling !== null) {
+    $(`#${params}`).nextElementSibling.remove(); //Após a mudança no campo remove esse aviso
   }
 }
