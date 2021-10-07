2ab18490fdbe30bcab69cfb0ddd0b30cb005ebc9
= <cassianoss@live.com>
2021-10-07 15:08:48 -0300

    fix: Consertado fluxo da validação do formulário

--- js/controller/FormsController.js
+++ js/controller/FormsController.js
@@ -18,17 +18,23 @@ class FormsController {
         if (validationForm1()) {
           this._chooseTab1.classList.remove("choose");
           this._chooseTab2.classList.add("choose");
+          this._tab1.classList.add("displayNone");
+          this._tab2.classList.remove("displayNone");
+          this._tab3.classList.add("displayNone");
           this._formNumber++;
         }
         break;
       case 2:
-        if(validationForm2()){
+        // if(validationForm2()){
         this._chooseTab2.classList.remove("choose");
         this._chooseTab3.classList.add("choose");
+        this._tab1.classList.add("displayNone");
+        this._tab2.classList.add("displayNone");
+        this._tab3.classList.remove("displayNone");
         this._btnNext.classList.add("displayNone");
         this._btnFinish.classList.remove("displayNone");
         this._formNumber++;
-        }
+        // }
         break;
     }
   }
